db7536ead0ca0a1be9eeca3bf9de49a80abb7cfd
Ameer Deen <ameer.deen@beeprojects.com>
2018-12-25 11:33:10 +1100

    First commit

---
+++ resource_server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceServer() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceServerCreate,
+		Read:   resourceServerRead,
+		Update: resourceServerUpdate,
+		Delete: resourceServerDelete,
+
+		Schema: map[string]*schema.Schema{
+			"address": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
+	log.Printf("Dumm Provider: create")
+	return resourceServerRead(d, m)
+}
+
+func resourceServerRead(d *schema.ResourceData, m interface{}) error {
+	log.Printf("Dumm Provider: read")
+	return nil
+}
+
+func resourceServerUpdate(d *schema.ResourceData, m interface{}) error {
+	log.Printf("Dumm Provider: update")
+
+	return resourceServerRead(d, m)
+}
+
+func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
+	log.Printf("Dumm Provider: update")
+	return nil
+}
